3f597905603dcb689edf2edc328d0beb0499caa7
root <fernando.bac03@hotmail.com>
2017-06-14 01:23:11 +0200

    add pagination

---
+++ ckanext/gallery/theme/public/scripts/forpagination.js
@@ -0,0 +1,18 @@
+
+$("#dataTable").simplePagination({
+	 
+	// the number of rows to show per page	perPage: 5,
+	 
+	// CSS classes to custom the pagination
+	containerClass: '',
+	previousButtonClass: '',
+	nextButtonClass: '',
+	 
+	// text for next and prev buttons
+	previousButtonText: 'Previous',
+	nextButtonText: 'Next',
+	 
+	// initial page
+	currentPage: 1
+	
+	});
