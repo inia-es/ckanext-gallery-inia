9bd4f9af2e55fc9e803e9b5e109c3d93e9ce874a
root <fernando.bac03@hotmail.com>
2017-06-14 11:39:57 +0200

    set new pagination

--- ckanext/gallery/theme/public/scripts/forpagination.js
+++ ckanext/gallery/theme/public/scripts/forpagination.js
@@ -1,4 +1,5 @@
 
-$(document).ready(function() {
-    $('#dataTable').DataTable();
-} );
+	$(document).ready(function(){		
+		$('#dataTable').tablePaginate({navigateType:'full',recordPerPage:2});
+	});
+
